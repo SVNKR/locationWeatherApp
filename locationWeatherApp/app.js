7724a23388dcc53791f14066f1b5a5a4bea1672b
submukherjee <Subhankar.Mukherjee@lexmark.com>
2018-05-31 19:32:50 +0530

    taking input from console

--- locationWeatherApp/app.js
+++ locationWeatherApp/app.js
@@ -5,10 +5,11 @@
 const geoService = require('./geoService');
 const weather = require('./weather');
 
-geoService.getCorordinates('belgharia').then((result) => {
+
+geoService.getCorordinates(process.argv[2]).then((result) => {
     //console.log(result.results[0].geometry.location.lat + "aaa" + result.results[0].geometry.location.lng );
     weather.getWeather(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng).then((wthrResult) => {
-            console.log(JSON.stringify(wthrResult));
+            console.log(JSON.stringify(wthrResult.currently));
 }, (error) => {
     console.log(`Weather details : ${JSON.stringify(error)}`);
 } );
