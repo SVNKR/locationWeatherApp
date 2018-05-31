a689e745bbb1f0f3b0bd3a330e7d21cb182399a4
submukherjee <Subhankar.Mukherjee@lexmark.com>
2018-05-31 19:28:16 +0530

    second commit

--- locationWeatherApp/app.js
+++ locationWeatherApp/app.js
@@ -3,12 +3,19 @@
  */
 
 const geoService = require('./geoService');
+const weather = require('./weather');
 
 geoService.getCorordinates('belgharia').then((result) => {
-    console.log(JSON.stringify(result.results[0].geometry.location));
+    //console.log(result.results[0].geometry.location.lat + "aaa" + result.results[0].geometry.location.lng );
+    weather.getWeather(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng).then((wthrResult) => {
+            console.log(JSON.stringify(wthrResult));
+}, (error) => {
+    console.log(`Weather details : ${JSON.stringify(error)}`);
+} );
+
         },
 (error) => {
-    console.log(JSON.stringify(error));
+    console.log(`Location details : ${JSON.stringify(error)}`);
 });
 
 
--- locationWeatherApp/weather.js
+++ locationWeatherApp/weather.js
@@ -1,3 +1,25 @@
 /**
  * Created by submukherjee on 5/31/2018.
  */
+
+const request =  require('request');
+
+var weather = {
+        getWeather : (lat, lang) => {
+        return new Promise(function(resolve, reject) {
+            request({
+                url : `https://api.darksky.net/forecast/0d915b856861f574aacd6cf1a223fdf1/${lat},${lang}`,
+                json : true
+            },  (error, response, body) => {
+                if(error) {
+                    reject(error);
+                }
+                else {
+                    resolve(body);
+        }
+        });
+        });
+}
+};
+
+module.exports = weather;
